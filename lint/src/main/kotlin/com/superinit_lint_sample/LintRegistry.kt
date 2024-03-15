0537428e590c5d3a960cddd2219dce128510c37e
ezhoon <dlwlgns1240@gmail.com>
2024-03-15 19:08:58 +0900

    CustomLintRegistry > LintRegistry

--- lint/src/main/kotlin/com/superinit_lint_sample/LintRegistry.kt
+++ lint/src/main/kotlin/com/superinit_lint_sample/LintRegistry.kt
@@ -3,7 +3,7 @@ package com.superinit_lint_sample
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
 
-class CustomLintRegistry : IssueRegistry() {
+class LintRegistry : IssueRegistry() {
 
     override val issues: List<Issue> = listOf(
         DtoDefaultValueDetector.ISSUE
