712132efcf9886ddab78317dd6dc1a955a9a1db6
ezhoon <dlwlgns1240@gmail.com>
2024-03-23 19:08:52 +0900

    Lint 코드 정리

--- lint/src/test/kotlin/com/superinit_lint_sample/DtoDefaultValueDetectorTest.kt
+++ lint/src/test/kotlin/com/superinit_lint_sample/DtoDefaultValueDetectorTest.kt
@@ -1,9 +1,7 @@
 package com.superinit_lint_sample
 
 import com.android.tools.lint.checks.infrastructure.LintDetectorTest
-import com.android.tools.lint.checks.infrastructure.TestFile
-import com.android.tools.lint.checks.infrastructure.TestLintResult
-import com.android.tools.lint.checks.infrastructure.TestMode
+import com.android.tools.lint.checks.infrastructure.TestLintTask
 import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Issue
 
@@ -13,6 +11,7 @@ import com.android.tools.lint.detector.api.Issue
  * - String 값이 비어있는 경우
  * - Class 값이 비어있는 경우
  * - kotlinx.serialization이 아닌 경우
+ * - kotlinx.serialization과 아닌 경우가 섞여져 있는 경우
  */
 class DtoDefaultValueDetectorTest : LintDetectorTest() {
 
@@ -22,6 +21,11 @@ class DtoDefaultValueDetectorTest : LintDetectorTest() {
 
     override fun getIssues(): MutableList<Issue> {
         return mutableListOf(DtoDefaultValueDetector.ISSUE)
+    }
+
+    override fun lint(): TestLintTask {
+        return super.lint()
+            .allowMissingSdk()
     }
 
     /**
@@ -37,7 +41,7 @@ class DtoDefaultValueDetectorTest : LintDetectorTest() {
     fun testSerializableWithOutDefaultValueInt() {
         val testFile = kotlin(
             """
-            package com.superinit_lint_sample.data.dto.api
+            package com.afreecatv.data.dto.api
             
             import kotlinx.serialization.Serializable
             
@@ -49,10 +53,12 @@ class DtoDefaultValueDetectorTest : LintDetectorTest() {
             """,
         ).indented()
 
-        runLintTaskSource(testFile)
+        lint()
+            .files(testFile, kotlinSerializable)
+            .run()
             .expect(
                 expectedText = """
-                src/com/superinit_lint_sample/data/dto/api/TestDataClassWithoutDefault.kt:7: Error: 해당 필드의 기본값을 할당해주세요. [DtoDefaultValueDetector]
+                src/com/afreecatv/data/dto/api/TestDataClassWithoutDefault.kt:7: Error: 해당 필드의 기본값을 할당해주세요 [DtoDefaultValueDetector]
                     val result: Int,
                     ~~~~~~~~~~~~~~~
                 1 errors, 0 warnings
@@ -62,7 +68,7 @@ class DtoDefaultValueDetectorTest : LintDetectorTest() {
                 fix = null,
                 after = kotlin(
                     """
-            package com.superinit_lint_sample.data.dto.api
+            package com.afreecatv.data.dto.api
 
             import kotlinx.serialization.Serializable
 
@@ -76,12 +82,199 @@ class DtoDefaultValueDetectorTest : LintDetectorTest() {
             )
     }
 
-    private fun runLintTaskSource(source: TestFile, stubs: Array<TestFile> = emptyArray()): TestLintResult =
+    /**
+     * given
+     *  - String 타입의 빈값을 갖고 있는 파일 제공
+     * when
+     *  - DtoDefaultValueDetector lint run()
+     * then
+     *  - DtoDefaultValueDetector의 lint로 잡힌다.
+     *  - expect 예상되는 메세지와 비교
+     *  - String값 = "" 로 기본값 세팅
+     */
+    fun testSerializableWithOutDefaultValueString() {
+        val testFile = kotlin(
+            """
+            package com.afreecatv.data.dto.api
+            
+            import kotlinx.serialization.Serializable
+            
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int = -1,
+                val message: String
+            )
+            """,
+        ).indented()
+
         lint()
-            .allowMissingSdk()
-            .allowCompilationErrors()
-            .skipTestModes(TestMode.FULLY_QUALIFIED)
-            .skipTestModes(TestMode.IMPORT_ALIAS)
-            .files(source, *stubs)
+            .files(testFile, kotlinSerializable)
             .run()
+            .expect(
+                expectedText = """
+                src/com/afreecatv/data/dto/api/TestDataClassWithoutDefault.kt:8: Error: 해당 필드의 기본값을 할당해주세요 [DtoDefaultValueDetector]
+                    val message: String
+                    ~~~~~~~~~~~~~~~~~~~
+                1 errors, 0 warnings
+                """.trimIndent(),
+            )
+            .checkFix(
+                fix = null,
+                after = kotlin(
+                    """
+            package com.afreecatv.data.dto.api
+
+            import kotlinx.serialization.Serializable
+
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int = -1,
+                val message: String = ""
+            )
+            """,
+                ).indented(),
+            )
+    }
+
+    /**
+     * given
+     *  - Class 타입의 빈값을 갖고 있는 파일 제공
+     * when
+     *  - DtoDefaultValueDetector lint run()
+     * then
+     *  - DtoDefaultValueDetector의 lint로 잡힌다.
+     *  - expect 예상되는 메세지와 비교
+     *  - Class는 = Class() 로 기본값 세팅
+     */
+    fun testSerializableWithOutDefaultValueCustomClass() {
+        val testFile = kotlin(
+            """
+            package com.afreecatv.data.dto.api
+            
+            import kotlinx.serialization.Serializable
+            
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int = -1,
+                val message: String = "",
+                val userFlag: UserFlag
+            )
+            """,
+        ).indented()
+
+        lint()
+            .files(testFile, kotlinSerializable)
+            .run()
+            .expect(
+                expectedText = """
+                src/com/afreecatv/data/dto/api/TestDataClassWithoutDefault.kt:9: Error: 해당 필드의 기본값을 할당해주세요 [DtoDefaultValueDetector]
+                    val userFlag: UserFlag
+                    ~~~~~~~~~~~~~~~~~~~~~~
+                1 errors, 0 warnings
+                """.trimIndent(),
+            )
+            .checkFix(
+                fix = null,
+                after = kotlin(
+                    """
+            package com.afreecatv.data.dto.api
+
+            import kotlinx.serialization.Serializable
+
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int = -1,
+                val message: String = "",
+                val userFlag: UserFlag = UserFlag()
+            )
+            """,
+                ).indented(),
+            )
+    }
+
+    /**
+     * given
+     *  - Kotrlinx의 Serializable이 아닌 Dto면서 기본값 없는 상태
+     * when
+     *  - DtoDefaultValueDetector lint run()
+     * then
+     *  - DtoDefaultValueDetector의 lint로 잡히지 않는다.
+
+     */
+    fun testNotSerializableDto() {
+        val testFile = kotlin(
+            """
+            package com.afreecatv.data.dto.api
+            
+            data class TestDataClassWithoutDefault(
+                val result: Int,
+                val message: String,
+                val userFlag: UserFlag
+            )
+            """,
+        ).indented()
+
+        lint()
+            .files(testFile, kotlinSerializable)
+            .run().expectClean()
+    }
+
+    /**
+     * given
+     *  - Kotrlinx의 Serializable이 아닌 DTO면서 기본값 없는 상태
+     *  - Kotrlinx의 Serializable인 DTO도 존재
+     * when
+     *  - DtoDefaultValueDetector lint run()
+     * then
+     *  - Kotrlinx의 Serializable인 DTO만 Lint로 잡혀야 한다.
+     */
+    fun testNotSerializableDtoAndSerializableDto() {
+        val testFile = kotlin(
+            """
+            package com.afreecatv.data.dto.api
+            
+            import kotlinx.serialization.Serializable
+            
+            data class TestDataWithoutSerializableDto(
+                val result: Int,
+                val message: String,
+                val userFlag: UserFlag
+            )
+
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int,
+                val message: String = "",
+                val userFlag: UserFlag = UserFlag()
+            )
+            """,
+        ).indented()
+
+        lint()
+            .files(testFile, kotlinSerializable)
+            .run()
+            .checkFix(
+                fix = null,
+                after = kotlin(
+                    """
+            package com.afreecatv.data.dto.api
+
+            import kotlinx.serialization.Serializable
+
+            data class TestDataWithoutSerializableDto(
+                val result: Int,
+                val message: String,
+                val userFlag: UserFlag
+            )
+
+            @Serializable
+            data class TestDataClassWithoutDefault(
+                val result: Int = 0,
+                val message: String = "",
+                val userFlag: UserFlag = UserFlag()
+            )
+            """,
+                ).indented(),
+            )
+    }
 }
